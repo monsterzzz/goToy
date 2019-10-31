6154c2de8e546213aac54a3bca24694dcb4a478a
monsterzzz <monsterxmonster@163.com>
2019-11-01 00:25:56 +0800

    缺失数

--- leetcode/main.go
+++ leetcode/main.go
@@ -4,24 +4,4 @@ import "fmt"
 
 func main() {
 
-	var pre = "1"
-	n := 4
-	for i := 2; i < n+1; i++ {
-		tmp := pre[0]
-		count := 1
-		tmpResult := ""
-		for j := 1; j < len(pre); j++ {
-			if pre[j] != tmp {
-				tmpResult += fmt.Sprintf("%d%s", count, string(tmp))
-				tmp = pre[j]
-				count = 1
-			} else {
-				count++
-			}
-		}
-		tmpResult += fmt.Sprintf("%d%s", count, string(tmp))
-		fmt.Println(i, pre, tmpResult)
-		pre = tmpResult
-	}
-
 }
