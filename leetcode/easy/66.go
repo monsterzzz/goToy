dbf0de1820fbb5c27f06e5b3af9c99ef6ecec3fa
monsterzzz <monsterxmonster@163.com>
2019-11-03 03:00:39 +0800

    加1

--- leetcode/easy/66.go
+++ leetcode/easy/66.go
@@ -2,12 +2,16 @@ package easy
 
 func plusOne(digits []int) []int {
 
-	for i := len(digits) - 1; i > 0; i++ {
-		if (digits[i]+1)%10 == 0 {
-			digits[i-1] += 1
-		} else {
-			digits[i] += 1
+	for i := len(digits) - 1; i >= 0; i-- {
+		digits[i] += 1
+		if digits[i] != 10 {
+			break
 		}
+		digits[i] = 0
+		if i == 0 {
+			digits = append([]int{1}, digits...)
+		}
+
 	}
 
 	return digits
