c2228fb0143ad2364b1bbeb6f434ba0c7a49235c
monsterzzz <monsterxmonster@163.com>
2019-11-04 01:03:46 +0800

    strStr

--- leetcode/main.go
+++ leetcode/main.go
@@ -2,34 +2,16 @@ package main
 
 import (
 	"fmt"
+	"goToy/leetcode/easy"
 )
 
 func main() {
 
-	s := "{[]}"
-
-	m := make(map[rune]rune)
-	m['('] = ')'
-	m['{'] = '}'
-	m['['] = ']'
-	l := make([]rune, 0)
-	for _, v := range s {
-		fmt.Println(l)
-		if len(l) == 0 {
-			l = append(l, v)
-			continue
-		}
-
-		if _, ok := m[l[len(l)-1]]; !ok {
-			return false
-		}
-
-		if m[l[len(l)-1]] == v {
-			//fmt.Println("!",l,string(l[len(l)-1]),string(m[l[len(l)-1]]),string(v))
-			l = l[:len(l)-1]
-		} else {
-			l = append(l, v)
-		}
-	}
+	s := "mississippi"
+	n := "issi"
+	//s:="aba"
+	//n:="aa"
+	fmt.Println(easy.StrStr(s, n))
+	//fmt.Println(len("The Sunday algorithm of "))
 
 }
