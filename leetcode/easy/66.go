2d27ba7ac17a92f18d2ebd9511e2ec82367fe6f3
monsterzzz <monsterxmonster@163.com>
2019-11-03 18:10:16 +0800

    加1

--- leetcode/easy/66.go
+++ leetcode/easy/66.go
@@ -2,6 +2,14 @@ package easy
 
 func plusOne(digits []int) []int {
 
+	/*
+
+		如果当前位加一不等于10,那么直接返回
+		如果等于10
+			把当前位取0
+			如果当前位还是首位 那么在首位加1
+
+	*/
 	for i := len(digits) - 1; i >= 0; i-- {
 		digits[i] += 1
 		if digits[i] != 10 {
--- leetcode/main.go
+++ leetcode/main.go
@@ -1,31 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"goToy/leetcode/easy"
+)
 
 func main() {
 
-	//digits1 := []int{1,2,3}
-	digits := []int{9}
+	s := "aa"
 
-	/*
+	fmt.Println(easy.IsPalindrome(s))
 
-		如果当前位加一不等于10,那么直接返回
-		如果等于10
-			把当前位取0
-			如果当前位还是首位 那么在首位加1
-
-	*/
-
-	for i := len(digits) - 1; i >= 0; i-- {
-		digits[i] += 1
-		if digits[i] != 10 {
-			break
-		}
-		digits[i] = 0
-		if i == 0 {
-			digits = append([]int{1}, digits...)
-		}
-
+	for _, v := range "0123456789" {
+		fmt.Println(v)
 	}
-	fmt.Println(digits)
 }
